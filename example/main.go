c0a5b2a5b678f702c8a885e9f661b33fc111ac15
ivandzf <divananda@ruangguru.com>
2020-10-20 15:54:12 +0700

    add example

--- example/main.go
+++ example/main.go
@@ -1,5 +1,30 @@
-package example
+package main
+
+import (
+	"fmt"
+	"image"
+	"os"
+
+	qrcodelogo "github.com/ivandzf/qrcode"
+)
 
 func main() {
+	f, err := os.Open("example/me.png")
+	if err != nil {
+		panic(err)
+	}
 
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	s, err := qrcodelogo.EncodeToBase64("https://github.com/ivandzf/qrcode-logo", img, 500)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("result: ", s)
 }
